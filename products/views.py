56f3b6dc12b2e4935b7357d85540e9a78c0459ce
Robin Romero <ingrobinromero@gmail.com>
2021-04-15 16:58:30 -0500

    version final

--- products/views.py
+++ products/views.py
@@ -7,6 +7,8 @@ from .models import Product
 # Create your views here.
 @login_required(login_url='/autenticacion/acceder')
 def listado_productos(request):
+    cart = Cart(request)
+    #cart.clear()
     products = Product.objects.all()
     return render(request, "products/listado.html", {
         "products": products
