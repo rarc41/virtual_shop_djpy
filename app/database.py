b8b8033b90008c4a248bb1d7cf519fd3371c6e08
Robin Romero <ingrobinromero@gmail.com>
2021-04-14 19:11:57 -0500

    commit 3

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 MYSQL = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'curso-django',
+        'NAME': 'virtual_shop',
         'USER': 'root',
         'PASSWORD': '',
         'PORT': '3306',
