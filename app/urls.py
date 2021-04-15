319da77b654a24cb575e4b07fb15329a656dfaf8
Robin Romero <ingrobinromero@gmail.com>
2021-04-15 17:19:28 -0500

    seteando pagina de inicio en la seccion de registro

--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', include('autenticacion.urls')),
     path('admin/', admin.site.urls),
     path('autenticacion/', include('autenticacion.urls')),
     path('products/', include('products.urls')),
--- autenticacion/urls.py
+++ autenticacion/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('',  VistaRegistro.as_view(), name='registro'),
     path('registro/', VistaRegistro.as_view(), name='registro'),
     path('salir/', salir, name='salir'),
     path('acceder/', acceder, name='acceder'),
